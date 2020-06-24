aa0ecffe65e52c9eacb02387647ed4687c3a0a85
Sera Aboagye <saboagye@flexera.com>
2020-06-24 18:30:11 -0500

    module fixed

--- dev/main.tf
+++ dev/main.tf
@@ -2,7 +2,7 @@ module "my_vpc" {
     souce = "../modules/vpc"
     vpc_cidr = "192.168.0.0/16"
     tenancy = "default"
-    vpc_id = "${module.vpc.vpc_id}"
+    vpc_id = "${module.my_vpc.vpc_id}"
     subnet_cidr = "192.168.1.0/24"
 }
 
@@ -10,7 +10,7 @@ module "my_ec2" {
     souce = "../modules/ec2"
     ec2_count = 1
     instance_type = "t2.micro"
-    subnet_id = "${module.vpc.subnet_id}""
+    subnet_id = "${module.my_vpc.subnet_id}"
     ami_id = ""
 }
 
