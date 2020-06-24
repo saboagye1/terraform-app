45d3f916cfe5eb590714dc30e6e8c61912aa75c7
Sera Aboagye <saboagye@flexera.com>
2020-06-24 18:24:17 -0500

    vars

--- Modules/ec2/vars.tf
+++ Modules/ec2/vars.tf
@@ -1,3 +1,11 @@
-variables "ami_id" {
-    default = ""
+variables "ami_id" {}
+
+variables "instance_type" {
+    default = "t2.micro"
+}
+
+variables "subnet_id" {}
+
+variables "ec2_count" {
+    default = "1"
 }
