4d6eec46a06dcee734b9b5e0e2861911c921b26e
Sera Aboagye <saboagye@flexera.com>
2020-06-24 18:24:10 -0500

    instances file updated

--- Modules/ec2/instances.tf
+++ Modules/ec2/instances.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "web" {
+  count = "${var.ec2_count}"
   ami           = "${var.ami_id}"
-  instance_type = "t2.micro"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${var.subnet_id}"
 
   tags = {
     Name = "HelloWorld"
